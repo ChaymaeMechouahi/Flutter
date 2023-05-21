e4213bfe6cfc8c58488c54deb0e09aa6b411d6c3
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2023-05-21 19:52:09 +0100

    blabla

--- lib/controllers/connexion.dart
+++ lib/controllers/connexion.dart
@@ -3,22 +3,16 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiEdition {
+  List<Edition> editions = [];
   Future<List<Edition>> editionsGet() async {
     http.Response response = await http.get(Uri.parse(
         "http://localhost:3005/editions")); // Obtenir les données de mon API
-    List<Edition> editions = [];
-
-    if (response.statusCode == 200) {
-      var body = jsonDecode(response.body);
-
-      for (var item in body) {
-        editions.add(Edition.fromJson(item));
-      }
-
-      return editions;
-    } else {
-      throw Exception(
-          'Failed to fetch editions'); // Lancer une exception en cas de problème avec la requête
+    
+    var body = jsonDecode(response.body);
+    for (var item in body) {
+      editions.add(Edition.fromJson(item));
     }
+
+    return editions;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hello/controllers/connexion.dart';
 import 'package:http/http.dart' as http;
@@ -11,34 +12,48 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  ApiEdition edition = ApiEdition();
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //ApiEdition edition = ApiEdition();
+  List<Edition> eds = [];
+  void fetchEditions() async {
+    print('fetchEditions called');
+    const url = 'http://localhost:3005/editions';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      eds = json.data;
+    });
+    print('done');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchEditions();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Editions App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Editions App'),
-        ),
-        body: FutureBuilder(
-          future: edition.editionsGet(),
-          builder: (context, snapshot) {
-            List<Edition>? eds = snapshot.data as List<Edition>?;
-            if (eds == null) {
-              return CircularProgressIndicator(); // ou un widget de chargement approprié
-            } else {
-              return ListView.builder(
-                itemCount: eds.length,
-                itemBuilder: (context, index) {
-                  return Text(eds[index].titre);
-                },
-              );
-            }
-          },
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Editions App'),
+          ),
+          body: ListView.builder(
+              itemCount: eds.length,
+              itemBuilder: (context, index) {
+                final edition = eds[index];
+                final titre = edition.getTitre;
+                return ListTile(title: Text(titre));
+              })),
     );
   }
 }
