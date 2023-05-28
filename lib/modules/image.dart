b72ba072aefec6ebea198cad5abe0e05f2e903da
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2023-05-28 16:10:33 +0100

    model

---
+++ lib/modules/image.dart
@@ -0,0 +1,30 @@
+class Edition {
+  //déclarer des variables non-nullable qui seront initialisées ultérieurement
+  late int id;
+  late int num_edition;
+  late String img;
+
+  Edition({
+    required this.id,
+    required this.num_edition,
+    required this.img,
+  });
+
+  Edition.fromJson(Map<String, dynamic> map) {
+    id = map['id'];
+    num_edition = map['num_edition'];
+    img = map['img'];
+  }
+
+  int get getId => id;
+
+  int get getNumEdition => num_edition;
+
+  String get getImg => img;
+
+  set setId(int value) => id = value;
+
+  set setNumero(int value) => num_edition = value;
+
+  set setImg(String value) => img = value;
+}
